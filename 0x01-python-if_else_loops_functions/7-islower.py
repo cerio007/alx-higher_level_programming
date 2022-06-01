a58a1e1b092ef80fb2ad1b53aebeed39797f46df
cerio007 <abosicyprian07@gmail.com>
2022-06-01 00:26:27 +0000

    first commit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for ord(c) in range(97, 122):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return(True)
+        if ord(c) >= 97 and ord(c) <= 122:
+            return(True)
     else:
         return(False)
