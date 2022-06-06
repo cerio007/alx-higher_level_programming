522060cc56760cedafcbf75474d50202f2307cb0
cerio007 <abosicyprian07@gmail.com>
2022-06-06 22:21:47 +0000

    first commit

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    length = len(my_string)
+
+    x = 0
+
+    new_string = my_string[:]
+
+    for i in range(length):
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
+            new_string = new_string[:(i - x)] + my_string[(i + 1):]
+            x += 1
+
+    return (new_string)
