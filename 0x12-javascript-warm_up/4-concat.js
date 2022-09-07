6d081c3e5800755f87b713f6433591328007ce4c
cerio007 <absicyprian07@gmail.com>
2022-09-07 14:35:32 +0000

    first commit

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-const myVar1 = prompt("enter a sentence");
-const myVar2 = prompt("enter another sentence");
-console.log(myVar1 + 'is' + myVar2);
+console.log(process.argv[2] + 'is' + process.argv[3]);
