ced215fb1c7a1dc66f0900b11e48a6024a54e95f
cerio007 <abosicyprian07@gmail.com>
2022-09-23 23:00:30 +0000

    fixed bug

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
         for i in range(0, 10):
             print("{}: {}".format(json_a[i].get('sha'), json_a[i].get('commit')
                                   .get('author').get('name')))
-    except IndexError:
+    except:
         pass
