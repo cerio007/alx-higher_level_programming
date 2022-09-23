9eafa40d70f424c317642fb8b19412417a56bf80
cerio007 <abosicyprian07@gmail.com>
2022-09-23 23:39:37 +0000

    check code

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == "__main__":
     try:
-        r = requests.get(sys.argv[1])
-        print(r.headers['X-Request-Id'])
+        a = requests.get(sys.argv[1])
+        print(a.header['X-Request-Id'])
     except:
         pass
