1df03ee83b4fb2034a202cb40ee0d343f61a2c1d
cerio007 <abosicyprian07@gmail.com>
2022-09-23 22:11:53 +0000

    fetches hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,6 @@
 """ Python script that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 
-
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
