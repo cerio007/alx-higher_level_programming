baf0e86e17d331f0f55df5dec65a44e03429944c
cerio007 <abosicyprian07@gmail.com>
2022-09-23 23:28:16 +0000

    check code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.url.open('https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(content)))
