e85a943b8121010f83ecea2b61ad1a9751a15c42
cerio007 <abosicyprian07@gmail.com>
2022-09-23 22:50:47 +0000

    fixed bug

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,13 +9,13 @@ import sys
 
 if __name__ == "__main__":
     try:
-        repo = sys.argv[1]
-        owner = sys.argv[2]
-        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-        r = get(url)
-        json_o = r.json()
+        repository_name = sys.argv[1]
+        owner_name = sys.argv[2]
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner_name, repository_name)
+       x = get(url)
+        json_a = x.json()
         for i in range(0, 10):
-            print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
+            print("{}: {}".format(json_a[i].get('sha'), json_a[i].get('commit')
                                   .get('author').get('name')))
     except:
         pass
