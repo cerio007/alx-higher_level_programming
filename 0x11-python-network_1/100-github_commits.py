6796033492877d8a9c8fc31878e00e6ff05002b3
cerio007 <abosicyprian07@gmail.com>
2022-09-24 00:25:26 +0000

    check code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Python script that shows thw last 10 commits of a GitHub
-repository.
 """
-import sys
+Python script that shows the last 10 commits of a repository
+in GitHub
+"""
 from requests import get, auth
+import sys
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
-
-    res = requests.get(url)
-    commit = res.json()
-    for i in range(10):
-        print("{}: {}".format(
-            commit[i].get("sha"), commit[i].get("commit").get("author").get("name")))
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner_name, repository_name)
+    res = get(url)
+    json_a = res.json()
+    for i in range(0, 10):
+        print("{}: {}".format(json_a[i].get('sha'), json_a[i].get('commit')
+                              .get('author').get('name')))
