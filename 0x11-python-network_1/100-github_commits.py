fde36e99f27a12cb2995e6ea03a9639dc1f9b0ce
cerio007 <abosicyprian07@gmail.com>
2022-09-24 00:19:02 +0000

    check code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+"""Python script that shows thw last 10 commits of a GitHub
+repository.
 """
-Python script that shows the last 10 commits of a repository
-in GitHub
-"""
-from requests import get, auth
 import sys
+from requests import get, auth
 
 
 if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    res = requests.get(url)
+    commit = res.json()
     try:
-        repository_name = sys.argv[1]
-        owner_name = sys.argv[2]
-        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner_name, repository_name)
-        x = get(url)
-        json_a = x.json()
-        for i in range(0, 10):
-            print("{}: {}".format(json_a[i].get('sha'), json_a[i].get('commit')
-                                  .get('author').get('name')))
+        for i in range(10):
+            print("{}: {}".format(
+                commit[i].get("sha"),
+                commit[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
